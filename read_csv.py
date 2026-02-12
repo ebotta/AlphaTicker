27bb14154ef73d920749b09ca6b2298cda959198
root <root@DESKTOP-LR4SMFQ.localdomain>
2026-02-12 16:42:13 -0500

    new

---
+++ read_csv.py
@@ -0,0 +1,11 @@
+import csv
+with open('output_data.csv', newline='') as in_file:
+    readlines = csv.reader(in_file, delimiter=',')
+    for line in readlines:
+#        print(line[0])
+        ticker=line[0].split(" ")
+#        print(ticker[0])
+        with open('tickers.csv', 'a', newline='') as out_file:
+            out_ticker = csv.writer(out_file)
+            out_ticker.writerow(ticker)
+
